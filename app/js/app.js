b588d0eaf65026afa9d4cbdcc29661425a100add
buschidos <sergey.snjozyk@gmail.com>
2015-12-15 16:23:07 +0200

    upd dist

--- app/js/app.js
+++ app/js/app.js
@@ -3,26 +3,27 @@
 window.$ = window.jQuery = require('jquery');
 require('slick-carousel');
 
-$('.hero-slider__slides').slick({
-    slide: '.hero-slide',
-    prevArrow: $('.hero-slider__prev'),
-    nextArrow: $('.hero-slider__next'),
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    useCSS: true
-});
-
-
-document.querySelector('.lang').addEventListener('click', function() {
-    this.classList.toggle('is-active');
-});
-
-[].slice.call(document.querySelectorAll('.dropdown')).forEach(function(element, index) {
-    element.addEventListener('click', function(e) {
+$(document).ready(function() {
+    document.querySelector('.lang').addEventListener('click', function() {
         this.classList.toggle('is-active');
-        if (e.target.tagName === 'LI') {
-            this.firstElementChild.firstChild.textContent = e.target.textContent + ' ';
-        }
+    });
+
+    [].slice.call(document.querySelectorAll('.dropdown')).forEach(function(element, index) {
+        element.addEventListener('click', function(e) {
+            this.classList.toggle('is-active');
+            if (e.target.tagName === 'LI') {
+                this.firstElementChild.firstChild.textContent = e.target.textContent + ' ';
+            }
+        });
+    });
+
+    $('.hero-slider__slides').slick({
+        slide: '.hero-slide',
+        prevArrow: $('.hero-slider__prev'),
+        nextArrow: $('.hero-slider__next'),
+        autoplaySpeed: 5000,
+        autoplay: true,
+        useCSS: true,
+        dots: true
     });
 });
